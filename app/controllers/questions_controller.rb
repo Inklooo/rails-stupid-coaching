f299e21f1920a89119cdfd1215a3a9fdc5e61c56
Loic Jeannin <inklo.ca@gmail.com>
2024-04-06 14:55:46 -0400

    stupid coaching on rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+
+    if params[:ask] == 'I am going to work'
+      @result = 'Great!'
+     elsif params[:ask].last == '?'
+       @result = 'Silly question, get dressed and go to work!'
+    else
+      @result = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
